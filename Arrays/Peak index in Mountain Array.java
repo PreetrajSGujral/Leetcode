b15e348668706707bbae0e81d9047e5f8c9123ad
PreetrajSGujral <44151163+RUGujral@users.noreply.github.com>
2019-01-16 22:57:13 -0500

    Create Peak index in Mountain Array.java

---
+++ Arrays/Peak index in Mountain Array.java
@@ -0,0 +1,24 @@
+class Solution {
+     public int print(int[] a, int beg, int end) {
+         if( beg<= end)
+         {
+        	 int mid =(beg+end)/2;
+        	 if(a[mid]>a[mid-1] && a[mid]>a[mid+1])
+        		 return mid;
+        	 if(a[mid]<a[mid+1])
+        	 {
+        		return print(a, mid+1, end);
+        	 }
+        	 if(a[mid]>a[mid+1])
+        	 {
+        		 return print(a, beg, mid-1);
+        	 }
+        	 
+         }
+         return -1;
+
+	 }
+    public int peakIndexInMountainArray(int[] A) {
+        return print(A, 0, A.length-1);
+    }
+}
